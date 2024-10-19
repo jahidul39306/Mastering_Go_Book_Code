4237c727e05e3d3512056a7976d40644080123de
jahidul39306 <jahidulislamananto@gmail.com>
2024-10-19 22:57:15 +0600

    for loop

--- chapter_1/hw/hw.go
+++ chapter_1/hw/hw.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+	a := []int{1, 2, 3, 4, -5}
+	// for i, v := range a {
+	// 	fmt.Println("Index: ", i, "value: ", v)
+	// }
+
+	i := 0
+	for {
+		if i >= len(a) {
+			break
+		}
+		fmt.Println("Index:", i, "value:", a[i])
+		i++
+	}
 }
