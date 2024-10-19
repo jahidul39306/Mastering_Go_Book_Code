8dd42995c475c4fd8234302afdb9421efad37c13
jahidul39306 <jahidulislamananto@gmail.com>
2024-10-19 23:06:23 +0600

    user inpt

--- chapter_1/hw/hw.go
+++ chapter_1/hw/hw.go
@@ -4,17 +4,24 @@ import "fmt"
 
 func main() {
 	// fmt.Println("Hello World")
-	a := []int{1, 2, 3, 4, -5}
+	// a := []int{1, 2, 3, 4, -5}
 	// for i, v := range a {
 	// 	fmt.Println("Index: ", i, "value: ", v)
 	// }
 
-	i := 0
-	for {
-		if i >= len(a) {
-			break
-		}
-		fmt.Println("Index:", i, "value:", a[i])
-		i++
-	}
+	// i := 0
+	// for {
+	// 	if i >= len(a) {
+	// 		break
+	// 	}
+	// 	fmt.Println("Index:", i, "value:", a[i])
+	// 	i++
+	// }
+
+	// user input
+
+	fmt.Printf("Please enter your name: ")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Println("Your name is:", name)
 }
